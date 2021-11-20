e6a8e96b14e0636ed1f225ee361c17f2c04aff83
Jens Ohlig <jens.ohlig@gmail.com>
2021-11-20 20:53:57 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import datetime
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    today = str(datetime.date.today().strftime("%d.%m.%Y"))
+    url = "https://programm.ard.de/programm/sender?sender=28106&datum=" + today
+    r = requests.get(url)
+    soup = soup = BeautifulSoup(r.text, "html.parser")
+    spans = soup.findAll("span")
+    titles = []
+    dates = []
+    for span in spans:
+        try:
+            classes = span["class"]
+            if "title" in classes:
+                s = str(span.contents[0]).strip()
+                if s:
+                    titles.append(s)
+            if "date" in classes:
+                s = str(span.contents[0]).strip()
+                if s:
+                    dates.append(s)
+        except KeyError:
+            """Ignore the tag that doesn't have a class atribute"""
+            pass
+    program = dict(zip(titles, dates))
+    when = "Die Tagesthemen kommen heute um " + program["Tagesthemen"] + " in der ARD."
+    return render_template("index.html", when=when, url=url)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=1024, debug=True)
