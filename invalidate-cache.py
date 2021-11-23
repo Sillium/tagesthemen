63fd74a2fa230e8f0a47c025aeeea0a589dca20e
Sillium <stoerk@gmx.de>
2021-11-23 21:43:50 +0000

    Add cache invalidate function

---
+++ invalidate-cache.py
@@ -0,0 +1,23 @@
+import os
+import datetime
+import logging
+import boto
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+client = boto3.client('apigateway')
+
+api_gateway_id = os.environ['API_GATEWAY_ID']
+stage = os.environ['STAGE']
+
+def run(event, context):
+    current_time = datetime.datetime.now().time()
+    name = context.function_name
+    logger.info("Your cron function " + name + " ran at " + str(current_time))
+    logger.info(f"Stage: {stage}, API Gateway Id: {api_gateway_id}")
+
+    #response = client.flush_stage_cache(
+    #    restApiId='string',
+    #    stageName='string'
+    #)
